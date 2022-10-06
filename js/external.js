6235d75090c037db16c82a75f0a107b58fac2085
Paul Wagner <p.wagner1120@icloud.com>
2022-10-06 11:29:54 -0500

    adding inline and external JS files

---
+++ js/external.js
@@ -0,0 +1,46 @@
+"use strict";
+
+console.log('Hello from external JavaScript')
+
+alert("Welcome to my Website");
+
+var userInput = prompt('What is your favorite color?');
+console.log('Great, ' + userInput,'is my favorite color too!');
+
+var confirmed = confirm('Scenario #1');
+console.log(confirmed); // will be either true or false
+
+    let littleMer = prompt('How many days did you have Little Mermaid');
+    let brotherB = prompt('How many days did you have Brother Bear');
+    let hercules = prompt('How many days did you have Hercules');
+    let pricePerDay = 3
+    let total = (Number(littleMer) + Number(brotherB) + Number(hercules))* pricePerDay;
+
+    alert('Your total is ' + total);
+
+var confirmed = confirm('Scenario #2');
+console.log(confirmed); // will be either true or false
+
+    let googlePay = 400;
+    let amazonPay = 380;
+    let fbPay = 350;
+    let googleHours = prompt('How many hours did you work for Google?');
+    let amazonHours = prompt('How many hours did you work for Amazon?');
+    let fbHours = prompt('How many hours did you work for Facebook?');
+
+    let paycheck = ('Gross pay is ' + (Number(googleHours * googlePay) + (amazonHours * amazonPay) + (fbHours * fbPay)));
+
+alert (paycheck);
+
+let classFull = confirm(`Is there room in the class and in your schedule?`);
+alert(`you can join the class: ${classFull}`);
+
+let premium = confirm(`Are you a premium member?`);
+alert(`You qualify for the product offer: ${premium}`);
+
+let itemsPurchased = confirm(`Are you buying more than 2 items and is the offer still valid?`);
+alert(`You qualify for the product offer: ${itemsPurchased}`);
+
+
+
+
