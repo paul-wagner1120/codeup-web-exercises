d51e5f8b4be60804a67e8c8f1d4ee7aa77fdb4c3
Paul Wagner <p.wagner1120@icloud.com>
2022-10-20 09:22:05 -0500

    adding warmups

---
+++ js/warmups.js
@@ -0,0 +1,18 @@
+
+
+    // Write a function that when passed an object will return the value of the object’s price property.
+    //     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
+    // getPrice(obj); // returns “$7.89”
+
+function getPrice(obj) {
+    return obj.price;
+}
+let potatoes = {
+    name: "Potatoes",
+    quantity: 2,
+    price: "$7.89"
+};
+
+    console.log(getPrice(potatoes));;
+
+
