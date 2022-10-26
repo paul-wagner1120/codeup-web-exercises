6c34ca69482d548c214d34c54161fe14b13c36f1
Paul Wagner <p.wagner1120@icloud.com>
2022-10-26 11:22:35 -0500

    adding login form

--- js/warmups.js
+++ js/warmups.js
@@ -33,3 +33,65 @@ function getLongestString(arr) {
 const colors = ["green", "blue", "periwinkle"]
 
     console.log(getLongestString(colors));
+
+// ----------------------------------------------------//
+
+const hamsters = [
+        {
+            name: "Hamtaro",
+            heightInMM: 86,
+            fur: ['orange', 'white'],
+            gender: "male",
+            dateOfBirth: "August 6"
+        } , {
+            name: "Bijou",
+            heightInMM: 75,
+            fur: ['white'],
+            gender: "female",
+            dateOfBirth: "July 10"
+        } , {
+            name: "Oxnard",
+            heightInMM: 100,
+            fur: ['grey', 'white'],
+            gender: "male",
+            dateOfBirth: "May 3"
+        } , {
+            name: "Boss",
+            heightInMM: 120,
+            fur: ['brown', 'white'],
+            gender: "male",
+            dateOfBirth: "September 21"
+        } , {
+            name: "Snoozer",
+            heightInMM: 85,
+            fur: ['brown', 'white', "pink"],
+            gender: "male",
+            dateOfBirth: "January 14"
+        }
+    ];
+
+function getTallestHamster(arr){
+    let tallest = {heightInMM: 0};
+    arr.forEach(function (el){ // for each loop
+        if(el.heightInMM > tallest.heightInMM) {
+            tallest = el;
+        }
+    });
+    // for(let i = 0; i < arr.length; i++) { // for loop
+    //     if(arr[i].heightInMM > tallest.heightInMM) {
+    //         tallest = arr[i];
+    //     }
+    // for (let el of arr) { // for of loop
+    //     if(el.heightInMM > tallest.heightInMM) {
+    //         tallest = el;
+    //     }
+
+
+    return tallest;
+}
+console.log(getTallestHamster(hamsters));
+
+// should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
+
+
+
