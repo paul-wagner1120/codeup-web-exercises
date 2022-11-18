655935add581d9e69a0bb89d2c7971948c015d09
Paul Wagner <p.wagner1120@icloud.com>
2022-11-18 10:47:16 -0600

    finished jquery mouse exercies

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,10 +1,27 @@
 "use strict"
 $(function() {
-$('.codeup').css('border', '1px solid red');
-$('li').css('font-size', '20px');
-$('h1, p, li').css('background', 'lightblue');
+// $('.codeup').css('border', '1px solid red');
+// $('li').css('font-size', '20px');
+// $('h1, p, li').css('background', 'lightblue');
+//
+// let alertH1 = $('h1').html();
+//     alert(alertH1);
 
-let alertH1 = $('h1').html();
-    alert(alertH1);
+    $('h1').click(function() {
+        $(this).css('background', 'limegreen');
+    });
+
+
+
+    $('p').dblclick(function(){
+        $(this).css('font-size', '18px');
+    });
+
+    $('li').hover(function() {
+        $(this).css('color', 'red');
+    }, function() {
+        $(this).css('color', 'black');
+    });
+
 });
 
