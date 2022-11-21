03f83fc4a9134ae69f3a966d6664da63e632c461
Paul Wagner <p.wagner1120@icloud.com>
2022-11-21 17:58:57 -0600

    finishing konami exc

--- js/konami.js
+++ js/konami.js
@@ -1,8 +1,9 @@
 "use strict";
 
 let cheatCode = [];
-let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]
-$(document).keyup(function (event) {
+const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]
+let myAudio = document.querySelector('#audio')
+$(document).keydown(function (event) {
     // console.log(event.keyCode);
     let keyPress;
 
@@ -11,23 +12,63 @@ $(document).keyup(function (event) {
         cheatCode.push(keyPress);
         console.log(cheatCode);
     }
+    switch(keyPress) {
+        case 38:
+            changeImg('img/d-pad-up.svg');
+            break;
+        case 40:
+            changeImg('img/d-pad-down.svg');
+            break;
+        case 37:
+            changeImg('img/d-pad-left.svg');
+            break;
+        case 39:
+            changeImg('img/d-pad-right.svg');
+            break;
+        case 66:
+            // $('.bButton').css("background", "lightgrey");
+            $('.bButton').css("opacity", "50%");
+            break;
+        case 65:
+            $('.aButton').css("opacity", "50%");
+            break;
+        case 13:
+            $('.startImg').css("opacity", "50%");
+    }
 
-
-
+function showImg(){
+        let img = document.createElement('img');
+        img.src = "img/dark-magician.png";
+        let src = document.getElementById("victory");
+        src.appendChild(img);
+}
 
     if (cheatCode.length === konamiCode.length && cheatCode.every((element, index) => element === konamiCode[index])) {
-        alert("Congrats, You now have 30 lives");
+        $('#victory').modal('show');
+        myAudio.play()
         cheatCode.length = 0;
     } else {
 
         for (let i = 0; i < cheatCode.length; i++) {
             if (cheatCode[i] !== konamiCode[i]) {
-                alert("Invalid code, Please try again");
+                // alert("Invalid code, Please try again");
                 cheatCode.length = 0;
             }
         }
     }
 });
+
+function changeImg(fileName){
+    let img = document.querySelector("#dpad");
+    img.setAttribute("src", fileName);
+}
+
+$(document).keyup(function (){
+    changeImg('img/d-pad-none.svg')
+    $('.bButton').css("opacity", "100%");
+    $('.aButton').css("opacity", "100%");
+    $('.startImg').css("opacity", "100%");
+})
 
 
 
