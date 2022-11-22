d1df728aeebef9fa38f1f094f9bffcc00829e77d
Paul Wagner <p.wagner1120@icloud.com>
2022-11-21 18:28:19 -0600

    adding comments to code

--- js/konami.js
+++ js/konami.js
@@ -4,15 +4,17 @@ let cheatCode = [];
 const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]
 let myAudio = document.querySelector('#audio')
 $(document).keydown(function (event) {
-    // console.log(event.keyCode);
     let keyPress;
 
+    //checking type of event
     if (typeof event !== 'undefined') {
         keyPress = event.keyCode;
         cheatCode.push(keyPress);
         console.log(cheatCode);
     }
-    switch(keyPress) {
+
+    //keydown events based on keyCode
+    switch (keyPress) {
         case 38:
             changeImg('img/d-pad-up.svg');
             break;
@@ -26,7 +28,6 @@ $(document).keydown(function (event) {
             changeImg('img/d-pad-right.svg');
             break;
         case 66:
-            // $('.bButton').css("background", "lightgrey");
             $('.bButton').css("opacity", "50%");
             break;
         case 65:
@@ -36,34 +37,29 @@ $(document).keydown(function (event) {
             $('.startImg').css("opacity", "50%");
     }
 
-function showImg(){
-        let img = document.createElement('img');
-        img.src = "img/dark-magician.png";
-        let src = document.getElementById("victory");
-        src.appendChild(img);
-}
-
+    //victory code, fires if konami code is entered correctly then sets input array to 0
     if (cheatCode.length === konamiCode.length && cheatCode.every((element, index) => element === konamiCode[index])) {
         $('#victory').modal('show');
         myAudio.play()
         cheatCode.length = 0;
     } else {
-
+        //checks for invalid code inputs and resets input array to 0
         for (let i = 0; i < cheatCode.length; i++) {
             if (cheatCode[i] !== konamiCode[i]) {
-                // alert("Invalid code, Please try again");
                 cheatCode.length = 0;
             }
         }
     }
 });
 
-function changeImg(fileName){
+//changes image status
+function changeImg(fileName) {
     let img = document.querySelector("#dpad");
     img.setAttribute("src", fileName);
 }
 
-$(document).keyup(function (){
+//catch all keyup function
+$(document).keyup(function () {
     changeImg('img/d-pad-none.svg')
     $('.bButton').css("opacity", "100%");
     $('.aButton').css("opacity", "100%");
