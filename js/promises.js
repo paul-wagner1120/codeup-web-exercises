f1eefbac19ce24b5dae352492874cb3575fca41f
Paul Wagner <p.wagner1120@icloud.com>
2022-12-08 14:25:31 -0600

    fixing IIFE

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,15 @@
-"use strict"
+(() => {
+"use strict";
+
 // got help with code structure from ralph in my breakout room.
 const lastGitCommit = (username) => {
-    return fetch(`https://api.github.com/users/${username}/events`, {headers: {Authorization: promisesAccessToken}})
+    return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {Authorization: promisesAccessToken}})
         .then(response => response.json())
         .then(events => events.filter(event => event.type === "PushEvent"))
         .then(pushEvents => console.log(pushEvents[0].created_at));
 };
 console.log(lastGitCommit('paul-wagner1120'));
+
+
+
+})();
