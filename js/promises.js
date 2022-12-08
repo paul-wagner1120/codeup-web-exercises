a96c6cda78eca3ec11ad00ed8b7129f240b5f073
Paul Wagner <p.wagner1120@icloud.com>
2022-12-08 11:49:38 -0600

    finished promises exercise

---
+++ js/promises.js
@@ -0,0 +1,9 @@
+"use strict"
+// got help with code structure from ralph in my breakout room.
+const lastGitCommit = (username) => {
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {Authorization: promisesAccessToken}})
+        .then(response => response.json())
+        .then(events => events.filter(event => event.type === "PushEvent"))
+        .then(pushEvents => console.log(pushEvents[0].created_at));
+};
+console.log(lastGitCommit('paul-wagner1120'));
